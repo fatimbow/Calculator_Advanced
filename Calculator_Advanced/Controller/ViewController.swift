aaaf6591977aed25a25a42aa5d43d25e145026ec
fatimbow <fatmabasaran217@gmail.com>
2023-07-12 14:59:07 +0300

    Add calculation methods.

---
+++ Calculator_Advanced/Controller/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Calculator_Advanced
+//
+//  Created by fatma y on 6.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var numCompleted: Bool = true
+    
+    var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Can not convert display label text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+        
+    }
+
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        
+        if let numValue = sender.currentTitle {
+            
+            if numCompleted {
+                displayLabel.text = numValue
+                numCompleted = false
+            } else {
+                if numValue == "." {
+                    let isInt = floor(displayValue) == displayValue
+                    if !isInt {
+                        return
+                    }
+                        
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
+        
+    }
+    
+    private var calculationLogic = CalculationLogic()
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        numCompleted = true
+        
+        calculationLogic.setNumber(displayValue)
+        
+        if let calcMethod = sender.currentTitle {
+            
+            if let result = calculationLogic.calc(symbol: calcMethod) {
+                displayValue = result
+            }
+    
+        }
+        
+    }
+    
+}
+
